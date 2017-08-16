28845485d70f9aa1183caae2c5e9045637dee672
Illia Ananich <illia.ananich@gmail.com>
2017-08-16 18:55:20 +0300

    add `testing spider, remove `gismeteo_*` spider

--- scrapy_climate/spiders/__init__.py
+++ scrapy_climate/spiders/__init__.py
@@ -1,4 +1,7 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+from scrapy_climate.tools.spider import TestingSpider
+
+
+class TSpider(TestingSpider):
+
+    name = 'testing'
+    start_urls = ['http://httpbin.org']
